9ffb06c588447bedb217faf6ac9c981b0709d47f
OutlyingWest <alexeybuv7@amail.com>
2024-05-07 21:01:12 +0300

    user auth views added

--- auth/views.py
+++ auth/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+from django.shortcuts import redirect
+from django.contrib import auth
+from django.http import HttpResponse
+from django.views.generic.edit import CreateView
 
-# Create your views here.
+
+class UserLoginView(LoginView):
+    template_name = 'login.html'
+    next_page = 'index'
+
+    def form_valid(self, form):
+        # Email log in alert message sending will be here
+        return super(UserLoginView, self).form_valid(form)
+
+
+class UserRegisterView(CreateView):
+    template_name = 'register.html'
+
+    def form_valid(self, form):
+        user = form.save()
+        auth.login(self.request, user)
+        return redirect('index')
+
+
+def change_password(request):
+    return HttpResponse('Этот обработчик меняет пароль пользователя')
+
+
+def reset_password(request):
+    return HttpResponse('Этот обработчик отвечает за сброс пароля пользователя')
