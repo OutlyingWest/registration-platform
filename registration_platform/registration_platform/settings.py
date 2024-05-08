fbe3b8e74c6916eebaae54d2d0220978c262f2ba
OutlyingWest <alexeybuv7@amail.com>
2024-05-08 11:43:10 +0300

    preparations to launch

--- registration_platform/registration_platform/settings.py
+++ registration_platform/registration_platform/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rvlhealm%9vbo%gkyjs3y0$3fp9(3()e8l#ekda2w0hb92=@v5'
+SECRET_KEY = os.environ.get('REGISTRATION_PROJECT_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Custom apps
+    'auth_app.apps.AuthAppConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,9 @@ ROOT_URLCONF = 'registration_platform.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,8 +79,15 @@ WSGI_APPLICATION = 'registration_platform.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'localhost',
+        'USER': os.environ.get('USER_DB'),
+        'PASSWORD': os.environ.get('PASSWORD_DB'),
+        'NAME': 'registration_platform',
+        # Unification db query in one transaction for a controller performing. Effective for multiple queries
+        'ATOMIC_REQUEST': False,
+        'AUTOCOMMIT': True,
+        'OPTIONS': {'charset': 'utf8mb4'},
     }
 }
 
@@ -103,21 +114,44 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
 USE_TZ = True
 
+SHORT_DATETIME_FORMAT = 'j.m.Y H:I'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'static'
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static/img',
+    BASE_DIR / 'static/styles',
+    BASE_DIR / 'static/scripts',
+]
+
+# Media files
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Use custom model instead the default
+AUTH_USER_MODEL = 'auth_app.User'
+
+
+# Authentication control
+# If user is not logged in, redirect to 'login' url
+LOGIN_URL = 'login'
+# If user authenticate successful, redirect to 'index' url
+LOGIN_REDIRECT_URL = 'index'
+# If user log out, redirect to 'logout' url
+LOGOUT_URL = 'logout'
