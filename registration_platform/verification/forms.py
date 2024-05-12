cd61104f7fbb44e8b49dcb32ac2e0915ca538fdd
OutlyingWest <alexeybuv7@amail.com>
2024-05-12 12:37:50 +0300

    account feature initials added

--- registration_platform/verification/forms.py
+++ registration_platform/verification/forms.py
@@ -1,12 +1,43 @@
 from django import forms
+from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.forms import modelformset_factory
 from .models import UserDocument
 
 
-class DocumentForm(forms.ModelForm):
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = get_user_model()
+        fields = (
+            'avatar',
+            'first_name',
+            'last_name',
+            'patronymic',
+            'gender',
+            'birthday',
+            'email',
+        )
+
+
+class UserDocumentForm(forms.ModelForm):
+    document_name_text = forms.CharField(
+        required=False,
+        label='',
+        widget=forms.TextInput(attrs={'readonly': True})
+    )
+
     class Meta:
         model = UserDocument
-        fields = ['document_name', 'status', 'file']
+        fields = (
+            'document_name',
+            'status',
+            'file',
+        )
+        widgets = {
+            'document_name': forms.HiddenInput(),
+            'file': forms.FileInput(attrs={'accept': 'application/pdf'}),
+            'status': forms.HiddenInput(),
+        }
 
 
-DocumentFormSet = modelformset_factory(UserDocument, form=DocumentForm, extra=0)
+UserDocumentsFormSet = modelformset_factory(UserDocument, form=UserDocumentForm, extra=0)
--- registration_platform/verification/views.py
+++ registration_platform/verification/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.views import View
 
-# Create your views here.
+from .forms import UserDocumentsFormSet, UserProfileForm
+from .models import UserDocument
+
+
+class UserAccountView(LoginRequiredMixin, View):
+    template_name = 'account.html'
+
+    def get(self, request, *args, **kwargs):
+        user_profile_form = UserProfileForm(instance=request.user)
+        user_documents_formset = UserDocumentsFormSet(queryset=UserDocument.objects.filter(user=request.user))
+        return render(request, self.template_name, {
+            'user_profile_form': user_profile_form,
+            'user_documents_formset': user_documents_formset,
+        })
+
+    def post(self, request, *args, **kwargs):
+        user_profile_form = UserProfileForm(request.POST, instance=request.user)
+        user_documents_formset = UserDocumentsFormSet(request.POST, request.FILES)
+
+        if user_profile_form.is_valid() and user_documents_formset.is_valid():
+            user_profile_form.save()
+            user_documents_formset.save()
+            return redirect(reverse('account'))
+
+        return render(request, self.template_name, {
+            'user_profile_form': user_profile_form,
+            'user_documents_formset': user_documents_formset,
+        })
