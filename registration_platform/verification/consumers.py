472d9e5da4c589a44ed355b686d70fd28fb0d946
OutlyingWest <alexeybuv7@amail.com>
2024-05-23 15:13:49 +0300

    channel groups improvements

--- registration_platform/verification/consumers.py
+++ registration_platform/verification/consumers.py
@@ -2,9 +2,6 @@ import json
 
 from channels.generic.websocket import AsyncWebsocketConsumer
 
-from channels.db import database_sync_to_async
-from channels.layers import get_channel_layer
-
 from .models import UserDocument
 from .services import document_status_send
 
@@ -12,7 +9,8 @@ from .services import document_status_send
 class DocumentStatusConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         await self.accept()
-        await self.channel_layer.group_add('document_status_update', self.channel_name)
+        user = self.scope['user']
+        await self.channel_layer.group_add(f'user_{user.id}_document_update', self.channel_name)
 
     async def disconnect(self, close_code):
         pass
@@ -21,8 +19,8 @@ class DocumentStatusConsumer(AsyncWebsocketConsumer):
         data = json.loads(text_data)
         document_id = int(data['document_id'].split('-')[1])
         await UserDocument.async_update_status_by_id(document_id, 'in_progress')
-        channel_layer = get_channel_layer()
-        await document_status_send(document_id, 'В обработке')
+        user = self.scope['user']
+        await document_status_send(user.id, document_id, 'В обработке')
 
     async def document_status_update(self, event):
         print(f'{event}')
@@ -30,6 +28,3 @@ class DocumentStatusConsumer(AsyncWebsocketConsumer):
             'document_id': event['document_id'],
             'new_status': event['new_status']
         }))
-
-
-
--- registration_platform/verification/services.py
+++ registration_platform/verification/services.py
@@ -10,14 +10,14 @@ async def verify_document(document: UserDocument):
     await asyncio.sleep(10)
     await document.async_update_status_by_id(document.id, 'approved')
     # Send status through WebSocket
-    await document_status_send(document.id, 'Одобрен')
+    await document_status_send(document.user.id, document.id, 'Одобрен')
     return document
 
 
-async def document_status_send(document_id: int, new_status: str):
+async def document_status_send(user_id: int, document_id: int, new_status: str):
     channel_layer = get_channel_layer()
     await channel_layer.group_send(
-        'document_status_update',  # Group
+        f'user_{user_id}_document_update',  # Group
         {
             'type': 'document.status.update',  # Method of consumer
             'document_id': document_id,
