a371ff8f4f211892f01d2656222854a635b4fc95
OutlyingWest <alexeybuv7@amail.com>
2024-05-28 23:10:36 +0300

    logger moved to class

--- registration_platform/verification/services.py
+++ registration_platform/verification/services.py
@@ -12,14 +12,11 @@ from pytesseract import Output
 from .models import UserDocument
 from .utilities import document_text_path
 
-logger = logging.getLogger(__name__)
-
 
 def verify_document(document_id: int):
     document = update_document_status(document_id, db_status='in_progress', frontend_status='В обработке')
 
     # Verification process placeholder
-    # time.sleep(10)
     recognizer = UserDocumentRecognizer(document)
     recognizer.recognize()
 
@@ -50,8 +47,10 @@ async def document_status_send(user_id: int, document_id: int, new_status: str, 
 
 class UserDocumentRecognizer:
     def __init__(self, document: UserDocument):
-        logger.info(f'User: {document.user.id}. Document: "{document.document_name}". Recognition process is started')
         self.document = document
+        self.logger = logging.getLogger(__name__)
+        self.logger.info(f'User: {document.user.id}.'
+                         f' Document: "{document.document_name}". Recognition process is started')
         self.text_path = self.set_text_path(document)
         self.page_title = f'# Page:'
         # Path to Tesseract executable, if not in std path
@@ -61,37 +60,39 @@ class UserDocumentRecognizer:
     def recognize(self):
         # Convert PDF to list of images (per page)
         images = convert_from_path(self.document.file.path, poppler_path=self.poppler_path)
+        pytesseract.pytesseract.tesseract_cmd = '/usr/local/bin/tesseract'
         for i, image in enumerate(images):
             current_page_title = f'{self.page_title}{i + 1}'
-            logger.info(f'Recognition of {current_page_title}...')
+            self.logger.info(f'Recognition of "{self.document.document_name}" {current_page_title}...')
 
             orientation = pytesseract.image_to_osd(image, output_type=Output.DICT)
             rotated_image = image.rotate(-orientation["rotate"], expand=True)
-            logger.debug(f'{orientation=}')
+            self.logger.debug(f'{orientation=}')
 
             orientation_rotated = pytesseract.image_to_osd(rotated_image, output_type=Output.DICT)
-            logger.debug(f'{orientation_rotated=}')
+            self.logger.debug(f'{orientation_rotated=}')
 
             raw_text = pytesseract.image_to_string(rotated_image, lang='rus')
             cleaned_text = '\n'.join(filter(bool, raw_text.split('\n')))
-            logger.debug(cleaned_text)
+            self.logger.debug(cleaned_text)
 
             self.append_text_to_file(current_page_title, cleaned_text)
+        self.logger.info(f'User: {self.document.user.id}.'
+                         f' Document: "{self.document.document_name}". Recognition process ended')
 
     def append_text_to_file(self, current_page_title: str, text: str):
         with open(self.text_path, 'a') as f:
             f.write(f'\n\n\n{current_page_title}\n')
             f.write(text)
 
-    @staticmethod
-    def set_text_path(document):
+    def set_text_path(self, document):
         text_path = document_text_path(document)
         directory = os.path.dirname(text_path)
         if not os.path.exists(directory):
             os.makedirs(directory)
         with open(text_path, 'w'):
             pass
-        logger.debug(f'{text_path=}')
+        # self.logger.debug(f'{text_path=}')
         return text_path
 
 
--- registration_platform/verification/tasks.py
+++ registration_platform/verification/tasks.py
@@ -7,21 +7,9 @@ from celery import shared_task
 from .services import document_status_send, verify_document
 
 
-@shared_task()
-def add(x, y):
-    return x + y
-
-
-@shared_task()
-def long(*args, **kwargs):
-    time.sleep(15)
-    async_to_sync(document_status_send(*args, **kwargs))
-    return 'Long Task Done!'
-
-
 @shared_task(
     soft_time_limit=60 * 5,
-    expires=60 * 7
+    expires=60 * 10
 )
 def verify_document_task(*args, **kwargs):
     verify_document(*args, **kwargs)
