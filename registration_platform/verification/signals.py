c914936ab3400eac111975bc28c884505eb4f52b
OutlyingWest <alexeybuv7@amail.com>
2024-06-08 13:14:57 +0300

    document creation made more readable

--- registration_platform/verification/signals.py
+++ registration_platform/verification/signals.py
@@ -8,4 +8,7 @@ from .models import UserDocument
 def create_user_documents(sender, instance, created, **kwargs):
     if created:
         for doc_type, doc_name in UserDocument.DOCUMENT_TYPES:
-            UserDocument.objects.create(user=instance, document_name=doc_type, status='not_uploaded')
+            UserDocument.objects.create(
+                user=instance,
+                document_name=doc_type,
+            )
