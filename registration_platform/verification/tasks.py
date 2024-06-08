b1e4df2c28379149eba3c37b37680918187a39b2
OutlyingWest <alexeybuv7@amail.com>
2024-06-08 13:19:32 +0300

    tasks decomposition started

--- registration_platform/verification/tasks.py
+++ registration_platform/verification/tasks.py
@@ -4,12 +4,33 @@ from asgiref.sync import async_to_sync
 
 from celery import shared_task
 
-from .services import document_status_send, verify_document
+from .services import verify_document, UserDocumentRecognizer, UserFullNameVerifier
 
 
 @shared_task(
     soft_time_limit=60 * 5,
-    expires=60 * 10
+    expires=60 * 10,
+    ignore_result=True,
 )
 def verify_document_task(*args, **kwargs):
     verify_document(*args, **kwargs)
+
+
+@shared_task(
+    soft_time_limit=60 * 5,
+    expires=60 * 10,
+    ignore_result=True,
+)
+def recognize_document_task(*args, **kwargs):
+    recognizer = UserDocumentRecognizer(*args, **kwargs)
+    recognizer.extract_text()
+
+
+@shared_task(
+    soft_time_limit=60 * 5,
+    expires=60 * 10,
+    ignore_result=True,
+)
+def verify_user_full_name_task(*args, **kwargs):
+    verifier = UserFullNameVerifier(*args, **kwargs)
+    verifier.verify()
