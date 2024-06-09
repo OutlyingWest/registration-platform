1aa9c7cc4a962a964029c96f76fb503c3eb7385f
OutlyingWest <alexeybuv7@amail.com>
2024-06-09 12:47:37 +0300

    verification verbose_name updated

--- registration_platform/verification/apps.py
+++ registration_platform/verification/apps.py
@@ -4,7 +4,7 @@ from django.apps import AppConfig
 class VerificationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'verification'
-    verbose_name = 'Верификация Документов'
+    verbose_name = 'Документы и Верификация'
 
     def ready(self):
         from .signals import create_user_documents
